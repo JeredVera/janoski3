5272f06d3e5eedde122356da5b7ee5e534c7196f
JeredVera <je.verap@duocuc.cl>
2023-08-16 13:11:05 -0400

    janoski

---
+++ src/app/app.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+@Component({
+  selector: 'app-root',
+  templateUrl: 'app.component.html',
+  styleUrls: ['app.component.scss'],
+})
+export class AppComponent {
+  public appPages = [
+    { title: 'Inicio', url: 'home', icon: 'home' },
+    { title: 'Jugadores', url: 'jugadores', icon: 'people-circle' },
+    { title: 'Equipos', url: 'equipos', icon: 'people' },
+    { title: 'Estadisticas', url: 'estadisticas', icon: 'analytics' },
+    { title: 'Campeon', url: 'campeon', icon: 'medal' },
+    { title: 'Fichaje', url: 'fichajes', icon: 'person-add' },
+  ];
+  public labels = ['Family', 'Friends', 'Notes', 'Work', 'Travel', 'Reminders'];
+  constructor() {}
+}
